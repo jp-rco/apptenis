c61c2ad6f9f56c743c0a03ca5c9327cb2c2121e8
MacaquinhoPro <andresriclo@unisabana.edu.co>
2025-01-24 14:00:20 -0500

    inicio

---
+++ app.java
@@ -0,0 +1,6 @@
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la aplicación de tenis!");
+        // Aquí puedes agregar la lógica de tu aplicación
+    }
+}
